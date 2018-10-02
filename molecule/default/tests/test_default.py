8c058d0eea611475a085cdbf9d930b7bdf299b13
Grzegorz Nowak <strange3studio@gmail.com>
2018-10-02 07:04:08 +0200

    also do symlinking to npm to be accessible globally, add a test to reflect that change

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -11,3 +11,7 @@ def test_node_version_ok(host):
     cmd = host.run("node -v")
     assert cmd.stderr == ''
     assert cmd.stdout == 'v8.11.3'
+
+def test_npm_ok(host):
+    cmd = host.run("npm -v")
+    assert cmd.stderr == ''
