37e94d8bddb36137d0eceba757d85bfe5dae2cd2
Grzegorz Nowak <strange3studio@gmail.com>
2018-10-02 15:07:47 +0200

    fix unit test lint

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,6 +12,7 @@ def test_node_version_ok(host):
     assert cmd.stderr == ''
     assert cmd.stdout == 'v8.11.3'
 
+
 def test_npm_ok(host):
     cmd = host.run("npm -v")
     assert cmd.stderr == ''
