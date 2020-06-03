7d2a146cafe5fb9668d5f59efdea047125e7c68b
Grzegorz Nowak <strange3studio@gmail.com>
2020-06-03 11:48:18 +0200

    stop relaying on handlers to install global packages, and improve the whole process in general. Better testing + include Focal Fossa as a supported target

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,7 +1,15 @@
 import os
+import time
 
 
 import testinfra.utils.ansible_runner
+
+# let the which-checking crontab run
+# anyone know better way of checking what cron thinks ? Please, do let me know!
+time.sleep(61)
+
+# man handle the damn 20.04's sudo bug...
+FOCAL_BUG_STR = 'sudo: setrlimit(RLIMIT_CORE): Operation not permitted\n'
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
@@ -10,33 +18,49 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_node_version_ok(host):
     cmd = host.run("node -v")
     assert cmd.stderr == ''
-    assert cmd.stdout.rstrip() == 'v8.11.3'
+    assert cmd.stdout.rstrip() == 'v10.16.3'
 
 
-def test_global_gulp_version_ok(host):
+def test_global_libraries(host):
     cmd = host.run("gulp -v")
     assert cmd.stderr == ''
     assert 'CLI version' in cmd.stdout
 
-    cmd = host.run("sudo -u www-data which gulp")
+    cmd = host.run("lighthouse --version")
     assert cmd.stderr == ''
-    assert cmd.stdout.rstrip() == '/usr/bin/gulp'
+    assert cmd.stdout != ''
+
+    with host.sudo("nvm_tester"):
+        cmd = host.run("which lighthouse")
+        assert cmd.stderr == '' or cmd.stderr == FOCAL_BUG_STR
+        assert cmd.stdout.rstrip() == '/usr/bin/lighthouse'
+
+        cmd = host.run("which gulp")
+        assert cmd.stderr == '' or cmd.stderr == FOCAL_BUG_STR
+        assert cmd.stdout.rstrip() == '/usr/bin/gulp'
+
+
+def test_cron_environment_for_global_libs(host):
+
+    # make sure cron sees global libraries in correct locations
+    assert host.run("cat /tmp/which_gulp").stdout.rstrip() == '/usr/bin/gulp'
+    assert host.run("cat /tmp/which_lighthouse").stdout.rstrip() == '/usr/bin/lighthouse'
+
+
+def test_user_node_version_ok(host):
+    with host.sudo("nvm_tester"):
+        cmd = host.run("node -v")
+        assert cmd.stderr == '' or cmd.stderr == FOCAL_BUG_STR
+        assert cmd.stdout.rstrip() == 'v10.16.3'
 
 
 def test_npm_ok(host):
     cmd = host.run("npm -v")
-    assert cmd.stdout.rstrip() == '5.6.0'
+    assert cmd.stdout.rstrip() == '6.9.0'
 
 
 def test_user_npm_ok(host):
     with host.sudo("nvm_tester"):
         host.check_output("whoami")
         cmd = host.run("npm -v")
-        assert cmd.stdout.rstrip() == '5.6.0'
-
-
-def test_user_node_version_ok(host):
-    with host.sudo("nvm_tester"):
-        cmd = host.run("node -v")
-        assert cmd.stderr == ''
-        assert cmd.stdout.rstrip() == 'v8.11.3'
+        assert cmd.stdout.rstrip() == '6.9.0'
